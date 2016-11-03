9302028000a12b64e8f8e80c0bee6c72e579b9da
Marco <MarcoRmz@users.noreply.github.com>
2016-11-03 17:45:42 -0600

    memory handler done

--- memoryHandler.py
+++ memoryHandler.py
@@ -9,6 +9,12 @@
 
 import memory
 
+#########################################
+#										#
+#         Variables for Memory          #
+#										#
+#########################################
+
 # Starting Virtual addresses for memory spaces
 # [INT, FLOAT, STRING, BOOL]
 currentGlobalVirtualAddress = [0, 1000, 2000, 3000]
@@ -29,23 +35,46 @@ invertedConstMemory = {}
 # Execution stack for memory
 memoryStack = []
 
-
-#										#
+#########################################
 #										#
 #        Functions for Compiler         #
 #										#
-#										#
+#########################################
 
+# Ask for next available memory address
+#Global
+def getGlobalAddress(varType, chunkSize):
+	availableAddress = currentGlobalVirtualAddress[varType]
+	currentGlobalVirtualAddress[varType] += chunkSize
+	return availableAddress
 
+# Constants
+def setConstantAddress(varType, varValue):
+	availableAddress = currentConstantVirtualAddress[varType]
+	currentConstantVirtualAddress[varType] += 1
+	invertedConstMemory[varValue] = availableAddress
+	constMemory[availableAddress] = varValue
+	return availableAddress
 
+# Local
+def getLocalAddress(varType, chunkSize):
+	availableAddress = currentLocalVirtualAddress[varType]
+	currentLocalVirtualAddress[varType] += chunkSize
+	return availableAddress
 
-
-
-#										#
+#########################################
 #										#
 #    Functions for Virtual Machine      #
 #										#
-#										#
+#########################################
+
+# Function to Create Memory for process
+def createMemory(TotalTypes, SubTypeQty):
+	memoryStack.append(Memory(TotalTypes, SubTypeQty))
+
+# Function to Delete process Memory
+def deleteMemory():
+	del memoryStack[-1]
 
 # Parse Virtual Address to Real Address and set new value
 # Global
