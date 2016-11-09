05171bead35b2535828a69e6f9afeb1392b3559e
andy <andy@localhost.localdomain>
2016-11-08 20:09:33 -0600

    blockly ,,,

--- blockly/blocks/custom.js
+++ blockly/blocks/custom.js
@@ -29,3 +29,106 @@ Blockly.Blocks['var_declaracion'] = {
     this.setHelpUrl('http://www.example.com/');
   }
 };
+
+//function_definition definition
+Blockly.Blocks['function_definition'] = {
+  init: function() {
+    this.appendValueInput("function_def")
+        .setCheck(null)
+        .appendField("function")
+        .appendField(new Blockly.FieldDropdown([["void", "void"], ["int", "int"], ["float", "float"], ["bool", "bool"], ["string", "string"]]), "function_type")
+        .appendField(new Blockly.FieldTextInput(""), "function_id");
+    this.appendStatementInput("function_body")
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField("}");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+//parameter1 (no closing) definition
+Blockly.Blocks['parameter1'] = {
+  init: function() {
+    this.appendValueInput("parameter1_value")
+        .setCheck(null)
+        .appendField("(")
+        .appendField(new Blockly.FieldDropdown([["int", "int"], ["float", "float"], ["bool", "bool"], ["string", "string"]]), "var_type")
+        .appendField(new Blockly.FieldTextInput(""), "var_name")
+        .appendField(",");
+    this.setOutput(true, null);
+    this.setColour(105);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+//parameter2 closing definition
+Blockly.Blocks['parameter2'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["int", "int"], ["float", "float"], ["bool", "bool"], ["string", "string"]]), "parameter2_types")
+        .appendField(new Blockly.FieldTextInput(""), "parameter2_value")
+        .appendField("){");
+    this.setOutput(true, null);
+    this.setColour(100);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+//empty_parameter definition
+Blockly.Blocks['empty_parameter'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("( ){");
+    this.setOutput(true, null);
+    this.setColour(100);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+//var_assign definition
+Blockly.Blocks['var_assign'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput(""), "var_id")
+        .appendField("=")
+        .appendField(new Blockly.FieldTextInput(""), "var_value");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(290);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+//main definition
+Blockly.Blocks['main'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("main {");
+    this.appendStatementInput("main_declaration")
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField("}");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+//endline definition
+Blockly.Blocks['endline'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
--- blockly/generators/javascript/custom.js
+++ blockly/generators/javascript/custom.js
@@ -13,3 +13,73 @@ Blockly.JavaScript['var_declaracion'] = function(block) {
   var code = dropdown_var_type + ' ' + text_var_id + '\n';
   return code;
 };
+
+//function_definition
+Blockly.JavaScript['function_definition'] = function(block) {
+  var dropdown_function_type = block.getFieldValue('function_type');
+  var text_function_id = block.getFieldValue('function_id');
+  var value_function_def = Blockly.JavaScript.valueToCode(block, 'function_def', Blockly.JavaScript.ORDER_ATOMIC);
+  var statements_function_body = Blockly.JavaScript.statementToCode(block, 'function_body');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'function '+dropdown_function_type+' '+text_function_id+''+value_function_def+' {\n'+statements_function_body+'}\n';
+  return code;
+};
+
+//parameter1 (no closing)
+Blockly.JavaScript['parameter1'] = function(block) {
+  var dropdown_var_type = block.getFieldValue('var_type');
+  var text_var_name = block.getFieldValue('var_name');
+  var value_parameter1_value = Blockly.JavaScript.valueToCode(block, 'parameter1_value', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var value_param1 = value_parameter1_value;
+  value_param1=value_param1.replace('(','');
+  value_param1=value_param1.replace(')','');
+  var code = dropdown_var_type+' '+text_var_name+','+value_param1;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+//parameter2 closing
+Blockly.JavaScript['parameter2'] = function(block) {
+  var dropdown_parameter2_types = block.getFieldValue('parameter2_types');
+  var text_parameter2_value = block.getFieldValue('parameter2_value');
+  // TODO: Assemble JavaScript into code variable.
+  var code = ' '+dropdown_parameter2_types+' '+text_parameter2_value;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+//empty parameter
+Blockly.JavaScript['empty_parameter'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '()';
+  //return code;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+//var_assign
+Blockly.JavaScript['var_assign'] = function(block) {
+  var text_var_id = block.getFieldValue('var_id');
+  var text_var_value = block.getFieldValue('var_value');
+  // TODO: Assemble JavaScript into code variable.
+  var code = text_var_id+' = '+text_var_value+'\n';
+  return code;
+};
+
+//main
+Blockly.JavaScript['main'] = function(block) {
+  var statements_main_declaration = Blockly.JavaScript.statementToCode(block, 'main_declaration');
+  // TODO: Assemble JavaScript into code variable.
+  var main_dec = statements_main_declaration.replace('(','')
+  main_dec = main_dec.replace(')','')
+  var code = 'main {\n' + main_dec+'\n}\n';
+  return code;
+};
+
+//endline
+Blockly.JavaScript['endline'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '\n';
+  return code;
+};
