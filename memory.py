049acbe723a149e77755b37e93ef98fd7776dc5f
Marco <MarcoRmz@users.noreply.github.com>
2016-11-09 11:46:12 -0600

    correct memory spaces

--- memory.py
+++ memory.py
@@ -16,8 +16,8 @@ class Memory:
 		count = 0
 		for i in range(0, 4):
 			if SubTypeQty[i] != 0:
-				self.memory[count] = [None for y in range(0, SubTypeQty[i])]
-				count += 1
+				self.memory[count] = [None] * SubTypeQty[i]
+			count += 1
 
 	def __del__(self):
 		class_name = self.__class__.__name__
